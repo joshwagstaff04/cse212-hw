f8b64002f636082ee5e7938742b2a7523052cc42
Josh Wagstaff <joshuawagstaff04@gmail.com>
2026-01-10 13:16:52 -0700

    fix .NET target and complete W01 Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,24 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        //Plan:
+        // 1 Make a new array of size length
+        //2 loop i from 0 to length -1
+        //3 Each spot is number * (i + 1)
+        //4 Reurn the array
 
-        return []; // replace this return statement with your own
+        var result = new double[length];
+
+        for (int i = 0; i < length; i++)
+
+        {
+            result[i] = number * (i + 1);
+        }
+
+
+
+
+        return result;
     }
 
     /// <summary>
@@ -25,9 +37,21 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        //i need to figure out where the list should be split
+        //everything at the end based on amount need to be moved to the front
+        int splitIndex = data.Count - amount;
+
+        //grab the two parts of the list
+        //the first part stays in order and the second part gets moved to the front
+        List<int> firstPart = data.GetRange(0, splitIndex);
+        List<int> secondPart = data.GetRange(splitIndex, amount);
+
+        //clear the list so it can be rebuilt
+        data.Clear();
+
+        //Put the end part first then the beginning part
+        data.AddRange(secondPart);
+        data.AddRange(firstPart);
+
     }
 }
